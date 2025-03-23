3d08c2e3f209cdb9e75d01a44af3b1cb0c1314e2
Iman Janghorban <iman.janghorban5@gmail.com>
2025-03-23 14:04:28 -0700

    Refactor Customer to follow SOLID principles

--- assessment-api-developer/Repositories/CustomersRepository.cs
+++ assessment-api-developer/Repositories/CustomersRepository.cs
@@ -1,58 +1,99 @@
 ﻿using assessment_platform_developer.Models;
+using assessment_platform_developer.Helper;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 
 namespace assessment_platform_developer.Repositories
 {
-	public interface ICustomerRepository
-	{
-		IEnumerable<Customer> GetAll();
-		Customer Get(int id);
-		void Add(Customer customer);
-		void Update(Customer customer);
-		void Delete(int id);
-	}
+    public interface ICustomerRepository
+    {
+        IEnumerable<Customer> GetAll();
+        Customer Get(int id);
+        void Add(Customer customer);
+        void Update(Customer customer);
+        void Delete(int id);
+    }
 
-	public class CustomerRepository : ICustomerRepository
-	{
-		// Assuming you have a DbContext named 'context'
-		private readonly List<Customer> customers = new List<Customer>();
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly CustomerDBContext _context;
 
-		public IEnumerable<Customer> GetAll()
-		{
-			return customers;
-		}
+        public CustomerRepository(CustomerDBContext context)
+        {
+            _context = context;
+        }
 
-		public Customer Get(int id)
-		{
-			return customers.FirstOrDefault(c => c.ID == id);
-		}
+        public IEnumerable<Customer> GetAll()
+        {
+            return _context.Customers.ToList();
+        }
 
-		public void Add(Customer customer)
-		{
-			customers.Add(customer);
-		}
+        public Customer Get(int id)
+        {
+            return _context.Customers.Find(id);
+        }
 
-		public void Update(Customer customer)
-		{
-			var existingCustomer = customers.FirstOrDefault(c => c.ID == customer.ID);
-			if (existingCustomer != null)
-			{
-				// Update properties of existingCustomer based on the properties of customer
-				// For example:
-				existingCustomer.Name = customer.Name;
-				// Repeat for other properties
-			}
-		}
+        public void Add(Customer customer)
+        {
+            // Validate customer data before adding
+            if (!ValidationHelper.ValidateCustomer(customer, out string errorMessage))
+            {
+                throw new ArgumentException(errorMessage);
+            }
 
-		public void Delete(int id)
-		{
-			var customer = customers.FirstOrDefault(c => c.ID == id);
-			if (customer != null)
-			{
-				customers.Remove(customer);
-			}
-		}
-	}
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+        }
+
+        public void Update(Customer customer)
+        {
+            // Validate customer data before updating
+            if (!ValidationHelper.ValidateCustomer(customer, out string errorMessage))
+            {
+                throw new ArgumentException(errorMessage);
+            }
+
+            var existingCustomer = _context.Customers.Find(customer.ID);
+            if (existingCustomer != null)
+            {
+                // Update all fields from the incoming customer object.
+                existingCustomer.Name = customer.Name;
+                existingCustomer.Address = customer.Address;
+                existingCustomer.Email = customer.Email;
+                existingCustomer.Phone = customer.Phone;
+                existingCustomer.City = customer.City;
+                existingCustomer.State = customer.State;
+                existingCustomer.Zip = customer.Zip;
+                existingCustomer.Country = customer.Country;
+                existingCustomer.Notes = customer.Notes;
+                existingCustomer.ContactName = customer.ContactName;
+                existingCustomer.ContactPhone = customer.ContactPhone;
+                existingCustomer.ContactEmail = customer.ContactEmail;
+                existingCustomer.ContactTitle = customer.ContactTitle;
+                existingCustomer.ContactNotes = customer.ContactNotes;
+
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Customer with ID {customer.ID} not found.");
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Customer with ID {id} not found.");
+            }
+        }
+    }
 }
