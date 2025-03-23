bafd9ee29d15117534d8670fa33454f8204b9673
Iman Janghorban <iman.janghorban5@gmail.com>
2025-03-23 14:06:40 -0700

    Update Customer Service to use the updated repository

--- assessment-api-developer/Services/CustomersService.cs
+++ assessment-api-developer/Services/CustomersService.cs
@@ -1,54 +1,64 @@
-﻿using assessment_platform_developer.Models;
+﻿using assessment_platform_developer.Helper;
+using assessment_platform_developer.Models;
 using assessment_platform_developer.Repositories;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace assessment_platform_developer.Services
 {
-	public interface ICustomerService
-	{
-		IEnumerable<Customer> GetAllCustomers();
-		Customer GetCustomer(int id);
-		void AddCustomer(Customer customer);
-		void UpdateCustomer(Customer customer);
-		void DeleteCustomer(int id);
-	}
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> GetAllCustomers();
+        Customer GetCustomer(int id);
+        void AddCustomer(Customer customer);
+        void UpdateCustomer(Customer customer);
+        void DeleteCustomer(int id);
+    }
 
-	public class CustomerService : ICustomerService
-	{
-		private readonly ICustomerRepository customerRepository;
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
 
-		public CustomerService(ICustomerRepository customerRepository)
-		{
-			this.customerRepository = customerRepository;
-		}
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
 
-		public IEnumerable<Customer> GetAllCustomers()
-		{
-			return customerRepository.GetAll();
-		}
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return _customerRepository.GetAll();
+        }
 
-		public Customer GetCustomer(int id)
-		{
-			return customerRepository.Get(id);
-		}
+        public Customer GetCustomer(int id)
+        {
+            return _customerRepository.Get(id);
+        }
 
-		public void AddCustomer(Customer customer)
-		{
-			customerRepository.Add(customer);
-		}
+        public void AddCustomer(Customer customer)
+        {
+            // Validate customer data
+            if (!ValidationHelper.ValidateCustomer(customer, out string errorMessage))
+            {
+                throw new ArgumentException(errorMessage);
+            }
 
-		public void UpdateCustomer(Customer customer)
-		{
-			customerRepository.Update(customer);
-		}
+            _customerRepository.Add(customer);
+        }
 
-		public void DeleteCustomer(int id)
-		{
-			customerRepository.Delete(id);
-		}
-	}
+        public void UpdateCustomer(Customer customer)
+        {
+            // Validate customer data
+            if (!ValidationHelper.ValidateCustomer(customer, out string errorMessage))
+            {
+                throw new ArgumentException(errorMessage);
+            }
 
+            _customerRepository.Update(customer);
+        }
+
+        public void DeleteCustomer(int id)
+        {
+            _customerRepository.Delete(id);
+        }
+    }
 }
